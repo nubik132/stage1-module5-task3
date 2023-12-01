af361e25e3e251e76618971e3b84dad8dbdb265d
nubik132 <mister.nubik@list.ru>
2023-12-01 13:42:34 +0300

    fix: delete import

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -1,13 +1,11 @@
 package com.epam.mjc;
 
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class CollectionService {
-
     public List<Integer> filterEvenNumbers(List<Integer> list) {
         return list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
     }
@@ -25,6 +23,6 @@ public class CollectionService {
     }
 
     public Integer sum(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().reduce(Integer::sum).get();
     }
 }
